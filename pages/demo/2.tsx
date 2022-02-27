88a3fdf85709583353f4e0ccc5aaabf47fc71aa5
ccn333 <benjamin.cacan@gmail.com>
2022-02-27 23:38:38 +0100

    demo test update

--- pages/demo/2.tsx
+++ pages/demo/2.tsx
@@ -1,22 +1,23 @@
 import MenuSVG from "components/MenuSVG/MenuSVG";
+import Screensaver from "components/Screensaver/Screensaver";
 import { useOnIdle } from "hooks/idleTimerHooks";
 import React, { useState } from "react";
 
 function Icon() {
-  const showControls = () => {
+  const showScreensaver = () => {
     setShowSS(false);
-    console.log("Showing controls");
+    console.log("Showing ss");
   };
-  const hideControls = () => {
+  const hideScreensaver = () => {
     setShowSS(true);
-    console.log("hiding controls");
+    console.log("hiding ss");
   };
 
   useOnIdle({
     debounce: 500,
-    idleTimeout: 1000,
-    onIdle: hideControls,
-    onAction: showControls,
+    idleTimeout: 10000,
+    onIdle: hideScreensaver,
+    onAction: showScreensaver,
     events: [
       "mousemove",
       "keydown",
@@ -31,9 +32,8 @@ function Icon() {
   const [showSS, setShowSS] = useState(false);
 
   return (
-    <div
-      style={{ background: showSS ? "white" : "black", width: "100%", height: "100%" }}
-    >
+    <div style={{ width: "100%", height: "100%" }}>
+      {showSS && <Screensaver />}
       <MenuSVG />
     </div>
   );
