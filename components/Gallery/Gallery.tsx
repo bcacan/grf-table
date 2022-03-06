bee3c65ce171d0de04fd9f79d76c777ec78ab738
ccn333 <benjamin.cacan@gmail.com>
2022-03-06 21:18:28 +0100

    gallery image fit fix

--- components/Gallery/Gallery.tsx
+++ components/Gallery/Gallery.tsx
@@ -4,39 +4,39 @@ import Image from "next/image";
 
 const photos: Photo[] = [
   {
-    src: "https://picsum.photos/200/300?image=1050",
-    width: 200,
-    height: 300,
+    src: "https://picsum.photos/900/550?image=1050",
+    width: 900,
+    height: 550,
     key: "1",
   },
   {
-    src: "https://picsum.photos/300/300?image=206",
-    width: 300,
-    height: 300,
+    src: "https://picsum.photos/800/900",
+    width: 800,
+    height: 900,
     key: "2",
   },
   {
-    src: "https://picsum.photos/200/300?image=1050",
-    width: 200,
-    height: 300,
+    src: "https://picsum.photos/600/1000",
+    width: 600,
+    height: 1000,
     key: "3",
   },
   {
-    src: "https://picsum.photos/200/300?image=1050",
-    width: 200,
-    height: 300,
+    src: "https://picsum.photos/1200/800",
+    width: 1200,
+    height: 800,
     key: "4",
   },
   {
-    src: "https://picsum.photos/300/300?image=206",
-    width: 300,
-    height: 300,
+    src: "https://picsum.photos/700/400",
+    width: 700,
+    height: 400,
     key: "5",
   },
   {
-    src: "https://picsum.photos/200/300?image=1050",
-    width: 200,
-    height: 300,
+    src: "https://picsum.photos/600/1300",
+    width: 650,
+    height: 1300,
     key: "6",
   },
 ];
@@ -69,8 +69,16 @@ const OpenImage = ({ img, closeImg }: any) => {
   const { src, width, height } = img;
   return (
     <>
-      <div onClick={closeImg}>close X</div>
-      <Image src={src} layout="fixed" width={width} height={height} />
+      <Image
+        src={src}
+        layout="fill"
+        /*width={width}  height={height}*/
+        objectFit="contain"
+      />
+
+      <div onClick={closeImg} style={{ position: "absolute", background: "black" }}>
+        close X
+      </div>
     </>
   );
 };
