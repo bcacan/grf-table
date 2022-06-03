0528469df8971b44117c4f67693c44ac671d8f6a
ccn333 <benjamin.cacan@gmail.com>
2022-06-03 02:53:06 +0200

    redirect

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,16 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/table");
+  }, []);
+
   return (
     <div className={styles.container}>
       <Head>
