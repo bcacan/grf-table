b1758d960cd1fe81a41ce129723381969906991a
bc <bc>
2022-06-08 13:58:08 +0200

    right click menu disabled

--- pages/table/index.tsx
+++ pages/table/index.tsx
@@ -44,6 +44,12 @@ const Table: NextPage = () => {
   const [showSS, setShowSS] = useState(true);
   /////
 
+  useEffect(() => {
+    document.addEventListener("contextmenu", (e) => {
+      e.preventDefault();
+    });
+  }, []);
+
   return (
     <React.StrictMode>
       <Head>
