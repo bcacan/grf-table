9d151a1ece788684532ea7301479b2be3a34aa14
ccn333 <benjamin.cacan@gmail.com>
2022-07-13 13:10:52 +0200

    device detector added

---
+++ components/DeviceDetector/DeviceDetector.tsx
@@ -0,0 +1,27 @@
+import { DeviceDetectorCSS } from "components/DeviceDetector/DeviceDetector.styled";
+import { useState } from "react";
+import { Text_Description } from "styles/texts";
+
+export default function DeviceDetector() {
+  const [deviceBlock, setDeviceBlock] = useState(true);
+
+  return (
+    <>
+      {deviceBlock ? (
+        <DeviceDetectorCSS>
+          <Text_Description id="m1">
+            You can't view this app on small screen.
+          </Text_Description>
+          <Text_Description id="m2">
+            It's not recomended to use app without touchscreen.
+          </Text_Description>
+
+          <button onClick={() => setDeviceBlock(false)}>Continue</button>
+        </DeviceDetectorCSS>
+      ) : (
+        <></>
+      )}
+      )
+    </>
+  );
+}
