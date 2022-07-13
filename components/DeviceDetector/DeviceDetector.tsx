cbba765254246277eec262aedfd5865abe935f5e
ccn333 <benjamin.cacan@gmail.com>
2022-07-13 13:20:47 +0200

    build hotfix

--- components/DeviceDetector/DeviceDetector.tsx
+++ components/DeviceDetector/DeviceDetector.tsx
@@ -10,10 +10,10 @@ export default function DeviceDetector() {
       {deviceBlock ? (
         <DeviceDetectorCSS>
           <Text_Description id="m1">
-            You can't view this app on small screen.
+            {" You can't view this app on small screen. "}
           </Text_Description>
           <Text_Description id="m2">
-            It's not recomended to use app without touchscreen.
+            {" It's not recomended to use app without touchscreen. "}
           </Text_Description>
 
           <button onClick={() => setDeviceBlock(false)}>Continue</button>
